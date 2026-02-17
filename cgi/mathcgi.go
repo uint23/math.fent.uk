c5826db61871ff5eddadc85b7b9250fc8d00609a
uint <abhinav.prsai@gmail.com>
2026-02-17 05:54:55 +0000

    Serve a proper page

--- cgi/mathcgi.go
+++ cgi/mathcgi.go
@@ -8,8 +8,39 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintln(w, "<h1>math.fent.uk</h1>")
-	fmt.Fprintln(w, "<p>ok</p>")
+
+	fmt.Fprint(w, `
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>mathfent</title>
+<link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+
+<h3>Daily Math Puzzles</h3>
+
+<p>
+Two equal circles, both of radius 5 rest on a line and are
+touching each other. A square sits between them as shown.
+Find <b><i>x</i></b>.
+</p>
+
+<img src="/static/example.png" alt="diagram">
+
+<details>
+<summary>Solution</summary>
+<p><i>x</i> = 2</strong>.</p>
+</details>
+
+<footer>
+footer
+</footer>
+
+</body>
+</html>
+`)
 }
 
 func main() {
